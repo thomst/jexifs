e760c848e0f879f84da632efe4690f4a1e83f601
Thomas Leichtfuss <thomaslfuss@gmx.de>
2013-08-22 09:31:15 +0200

    first commit:
    changed the design of Image. But now its more slowly. So I will rechange
    it again.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import os
+
+VERSION = "0.1.0"
+
+setup( 
+    name = "jexifs", 
+    version = VERSION, 
+    author = "Thomas Leichtfuss", 
+    author_email = "thomaslfuss@gmx.de",
+    url = "https://github.com/thomst/jexifs",
+    download_url = "https://pypi.python.org/packages/source/j/jexifs/jexifs-{version}.tar.gz".format(version=VERSION),
+    description = 'Select jpegs based on their exif-data.',
+    long_description = open('README.rst').read() if os.path.isfile('README.rst') else str(),
+    scripts = ["jexifs"],
+    install_requires = ['timeparse'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    license='GPL',
+    keywords='exif exifdata jpeg time batch selection',
+)
